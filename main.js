db619a980c04b487db5171f57e5ff0bf48c821b6
Jan Edmund Lazo <jan.lazo@mail.utoronto.ca>
2018-01-24 13:06:31 -0500

    Phase1: getCollection() done

--- main.js
+++ main.js
@@ -1,13 +1,82 @@
-const request = require('request')
+#!/usr/bin/env node
+const request = require('request');
+const xml2js = require('xml2js');
 
+let apikey = '';
+let username = '';
+let password = '';
+let cmd_args = process.argv.slice(2);
 
+while (cmd_args.length > 0) {
+  cur_arg = cmd_args.shift();
+
+  switch (cur_arg) {
+    case '--apikey':
+      apikey = cmd_args.shift();
+      break;
+    case '--username':
+      username = cmd_args.shift();
+      break;
+    case '--password':
+      password = cmd_args.shift();
+      break;
+  }
+}
+
+if (!(apikey && username && password)) {
+  console.error('Missing apikey, username, or password');
+  process.exit(1);
+}
 
 function getCollection(callback) {
-  // implement
+  request.post({
+    url: 'https://pastebin.com/api/api_login.php',
+    form: {
+      api_dev_key: apikey,
+      api_user_name: username,
+      api_user_password: password
+    }
+  }, function (error, response, body) {
+    if (error) {
+      console.error('Invalid apikey, username, or password');
+      return;
+    }
+
+    let userkey = body;
+    request.post({
+      url: 'https://pastebin.com/api/api_post.php',
+      form: {
+        api_dev_key: apikey,
+        api_user_key: userkey,
+        api_option: 'list'
+      }
+    }, function (error, response, body) {
+      if (error) {
+        console.error('Unknown error');
+        return;
+      }
+
+      xml2js.parseString(body, function (error, result) {
+        if (error) {
+          console.error('Cannot parse output as XML');
+          return;
+        }
+
+        if (!(typeof result === 'object' &&
+              typeof result.paste === 'object')) {
+          console.error('User has no pastes');
+          return;
+        }
+
+        callback(Array.isArray(result.paste) ? result.paste : [result.paste]);
+      });
+    });
+  });
 }
 
 function getSortedCollection(callback) {
   // implement
+  getCollection(callback);
 }
 
 // use the following to test your functions
@@ -16,6 +85,6 @@ getCollection(function(array){
   console.log("First item in collection ",array[0])
 })
 
-getSortedCollection(function(array) {
-  console.log("Collection sorted ",array)
-})
+// getSortedCollection(function(array) {
+//   console.log("Collection sorted ",array)
+// })
